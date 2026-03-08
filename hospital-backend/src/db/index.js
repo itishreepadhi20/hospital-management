60551b7120f9d3a2b052d8d69afd98bc2c215eeb
Itishree padhi <itish@example.com>
2026-03-08 10:57:33 +0530

    insert app.js file

--- hospital-backend/src/db/index.js
+++ hospital-backend/src/db/index.js
@@ -1,38 +1,15 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
-
-dotenv.config({
-    path:'./.env'
-})
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
 
 
-
-
-
-
-
-
-
-
-/*
-import express from "express"
-const app = express()
-( async () => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
     }
-})()
+}
 
-*/
+export default connectDB
