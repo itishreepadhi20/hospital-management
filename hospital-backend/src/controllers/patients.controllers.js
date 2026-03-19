811810c7d73ff880902ed069d6ea53fce28fe70a
Itishree padhi <itish@example.com>
2026-03-19 10:14:42 +0530

    add logoutPatient function to handle patient logout

--- hospital-backend/src/controllers/patients.controllers.js
+++ hospital-backend/src/controllers/patients.controllers.js
@@ -150,11 +150,36 @@ const loginPatient=asyncHandler(async(req,res)=>{
             "patient logged In Successfully"
         )
     )
+  
+})
 
-    
+const logoutPatient=asyncHandler(async(req,res)=>{
+    await Patient.findByIdAndUpdate(
+        req.patient._id,
+        {
+            $unset:{
+                refreshToken:1
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"Patient loggedout"))
 })
 
 export {
   registerPatient,
-  loginPatient
+  loginPatient,
+  logoutPatient
 }
